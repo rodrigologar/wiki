c4501d891547ca7e1a03cdaddaba840587810b7b
rodrigologar <rodrigo.logar02@gmail.com>
2021-09-08 19:10:19 -0500

    finished search and started new page creator

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entries, name="entries")
+    path("wiki/<str:title>", views.entries, name="entries"),
+    path("search/", views.search, name="search"),
+    path("new_page/", views.new_page, name="new_page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import request
+from django.shortcuts import redirect, render
+from django import forms
 
 from . import methods
 from . import util
 
-
-def index(request):
+def index(request):    
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
@@ -15,4 +15,27 @@ def entries(request, title):
         "title": title,
         "entry": methods.mdConversion(title)
     })
+    
+def search(request):
+    searched_entry = request.GET['q']
+    
+    if util.get_entry(searched_entry) != None:
+        return render(request, "encyclopedia/entries.html", {
+            "title":searched_entry,
+            "entry":methods.mdConversion(searched_entry)
+        })
+        
+    else:
+        entries = util.list_entries()
+        results = []
+        for entry in entries:
+            if searched_entry in entry.lower():
+                results.append(entry)
+        
+        return render(request, "encyclopedia/results.html", {
+            "results":results
+        })
+        
+def new_page(request):
+    return render(request, "encyclopedia/new_page.html")
 
